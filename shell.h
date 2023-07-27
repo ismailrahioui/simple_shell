8ee2439e434146436a8d1f9701a542dd32632551
Fadymaged129 <113293968+Fadymaged129@users.noreply.github.com>
2023-07-28 00:41:16 +0200

    Update shell.h

--- shell.h
+++ shell.h
@@ -112,97 +112,102 @@ typedef struct builtin
 
 
 /* toem_shloop.c */
-int hsh(info_t *, char **);
-int find_builtin(info_t *);
-void find_cmd(info_t *);
-void fork_cmd(info_t *);
+int hsh(info_t *soha, char **avat);
+int find_builtin(info_t *soha);
+void find_cmd(info_t *soha);
+void fork_cmd(info_t *soha);
 
 /* toem_parser.c */
-int is_cmd(info_t *, char *);
-char *dup_chars(char *, int, int);
-char *find_path(info_t *, char *, char *);
+int is_cmd(info_t *soha, char *vini);
+char *dup_chars(char *vinistr, int ebda, int ends);
+char *find_path(info_t *soha, char *vinistr, char *kfc);
 
 /* loophsh.c */
 int loophsh(char **);
 
 /* toem_errors.c */
-void _eputs(char *);
-int _eputchar(char);
-int _putfd(char c, int fd);
-int _putsfd(char *str, int fd);
+void _eputs(char *storz);
+int _eputchar(char cur);
+int _putfd(char mac, int bet);
+int _putsfd(char *storz, int bet);
 
 /* toem_string.c */
-int _strlen(char *);
-int _strcmp(char *, char *);
-char *starts_with(const char *, const char *);
-char *_strcat(char *, char *);
+int _strlen(char *soso);
+int _strcmp(char *soso1, char *soso2);
+char *starts_with(const char *haystuck, const char *noodle);
+char *_strcat(char *desk, char *srk);
 
 /* toem_string1.c */
-char *_strcpy(char *, char *);
-char *_strdup(const char *);
-void _puts(char *);
-int _putchar(char);
+char *_strcpy(char *desk, char *srk);
+char *_strdup(const char *stfield);
+void _puts(char *stfield);
+int _putchar(char k);
 
 /* toem_exits.c */
-char *_strncpy(char *, char *, int);
-char *_strncat(char *, char *, int);
-char *_strchr(char *, char);
+char *_strncpy(char *dgst, char *surc, int nun);
+char *_strncat(char *dgst, char *surc, int nun);
+char *_strchr(char *sos, char cue);
 
 /* toem_tokenizer.c */
-char **strtow(char *, char *);
-char **strtow2(char *, char);
+char **strtow(char *FD, char *d);
+char **strtow2(char *FD, chard);
 
 /* toem_realloc.c */
-char *_memset(char *, char, unsigned int);
-void ffree(char **);
-void *_realloc(void *, unsigned int, unsigned int);
+char *_memset(char *soso, char book, unsigned int noon);
+void ffree(char **poop);
+void *_realloc(void *pter, unsigned int osize, unsigned int nsize);
 
 /* toem_memory.c */
 int bfree(void **);
 
 /* toem_atoi.c */
-int interactive(info_t *);
-int is_delim(char, char *);
-int _isalpha(int);
-int _atoi(char *);
+int interactive(info_t *tach);
+int is_delim(char ci, char *deliman);
+int _isalpha(int cio);
+int _atoi(char *ese);
 
 /* toem_errors1.c */
-int _erratoi(char *);
-void print_error(info_t *, char *);
-int print_d(int, int);
-char *convert_number(long int, int, int);
-void remove_comments(char *);
+int _erratoi(char *seen);
+void print_error(info_t *tach, char *tesr);
+int print_d(int value, int bet);
+char *convert_number(long int numero, int bas, int signes);
+void remove_comments(char *bu);
 
 /* toem_builtin.c */
-int _myexit(info_t *);
-int _mycd(info_t *);
-int _myhelp(info_t *);
+int _myexit(info_t *tach);
+int _mycd(info_t *tach);
+int _myhelp(info_t *tach);
 
 /* toem_builtin1.c */
-int _myhistory(info_t *);
-int _myalias(info_t *);
+int _myhistory(info_t *tach);
+int unset_alias(info_t *tach, char *store);
+int set_alias(info_t *tach, char *store);
+int print_alias(list_t *nodejs);
+int _myalias(info_t *tach);
 
 /*toem_getline.c */
-ssize_t get_input(info_t *);
-int _getline(info_t *, char **, size_t *);
-void sigintHandler(int);
+ssize_t input_buf(info_t *tach, char **bu, size_t *leno);
+ssize_t get_input(info_t *tach);
+ssize_t read_buf(info_t *tach, char *bu, size_t *size);
+int _getline(info_t *tach, char **pitro, size_t *leno);
+void sigintHandler(__attribute__((unused))int s_num);
 
 /* toem_getinfo.c */
-void clear_info(info_t *);
-void set_info(info_t *, char **);
-void free_info(info_t *, int);
+void clear_info(info_t *tazeko);
+void set_info(info_t *otipax, char **avenue);
+void free_info(info_t *cea, int alli);
 
 /* toem_environ.c */
-char *_getenv(info_t *, const char *);
-int _myenv(info_t *);
-int _mysetenv(info_t *);
-int _myunsetenv(info_t *);
-int populate_env_list(info_t *);
+char *_getenv(info_t *tach, const char *nom);
+int _myenv(info_t *tach);
+int _mysetenv(info_t *tach);
+int _myunsetenv(info_t *tach);
+int populate_env_list(info_t *tach);
 
 /* toem_getenv.c */
-char **get_environ(info_t *);
-int _unsetenv(info_t *, char *);
-int _setenv(info_t *, char *, char *);
+char **get_environ(info_t *tach);
+int _unsetenv(info_t *tach, char *vary);
+int _setenv(info_t *tach, char *very, char *input);
 
 /* toem_history.c */
 char *get_history_file(info_t *info);
@@ -212,25 +217,25 @@ int build_history_list(info_t *info, char *buf, int linecount);
 int renumber_history(info_t *info);
 
 /* toem_lists.c */
-list_t *add_node(list_t **, const char *, int);
-list_t *add_node_end(list_t **, const char *, int);
-size_t print_list_str(const list_t *);
-int delete_node_at_index(list_t **, unsigned int);
-void free_list(list_t **);
+list_t *add_node(list_t **addpoint, const char *stfield, int raqm);
+list_t *add_node_end(list_t **addpoint, const char *stfield, int raqm);
+size_t print_list_str(const list_t *x);
+int delete_node_at_index(list_t **addpoint, unsigned int inteex);
+void free_list(list_t **heaptr);
 
 /* toem_lists1.c */
-size_t list_len(const list_t *);
-char **list_to_strings(list_t *);
-size_t print_list(const list_t *);
-list_t *node_starts_with(list_t *, char *, char);
-ssize_t get_node_index(list_t *, list_t *);
+size_t list_len(const list_t *x);
+char **list_to_strings(list_t *addpoint);
+size_t print_list(const list_t *x);
+list_t *node_starts_with(list_t *noote, char *repfox, char k);
+ssize_t get_node_index(list_t *addpoint, list_t *noote);
 
 /* toem_vars.c */
-int is_chain(info_t *, char *, size_t *);
-void check_chain(info_t *, char *, size_t *, size_t, size_t);
-int replace_alias(info_t *);
-int replace_vars(info_t *);
-int replace_string(char **, char *);
+int is_chain(info_t *tech, char *buffer, size_t *pi);
+void check_chain(info_t *tech, char *buffer, size_t *pi, size_t i, size_t len);
+int replace_alias(info_t *tech);
+int replace_vars(info_t *tech);
+int replace_string(char **adim, char *gedid);
 
 #endif
 
