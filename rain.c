59a35bccb5b1cdf22dc07557a2d78a6bd98e6bd2
Ismail Rahioui <89405417+ismailrahioui@users.noreply.github.com>
2023-07-27 08:15:50 +0100

    last dance

--- rain.c
+++ rain.c
@@ -10,36 +10,38 @@
 */
 ssize_t input_buf(info_t *tach, char **bu, size_t *leno)
 {
-ssize_t r = 0;
-size_t len_p = 0;
+	ssize_t r = 0;
+	size_t len_p = 0;
 
-if (!*leno)
-{
-free(*bu);
-*bu = NULL;
-signal(SIGINT, sigintHandler);
+	if (!*leno) /* if nothing left in the buffer, fill it */
+	{
+		/*bfree((void **)tach->cmd_buf);*/
+		free(*bu);
+		*bu = NULL;
+		signal(SIGINT, sigintHandler);
 #if USE_GETLINE
-r = getline(bu, &len_p, stdin);
+		r = getline(bu, &len_p, stdin);
 #else
-r = _getline(tach, bu, &len_p);
+		r = _getline(tach, bu, &len_p);
 #endif
-if (r > 0)
-{
-if ((*bu)[r - 1] == '\n')
-{
-(*bu)[r - 1] = '\0';
-r--;
-}
-tach->linecount_flag = 1;
-remove_comments(*bu);
-build_history_list(tach, *bu, tach->histcount++);
-{
-*leno = r;
-tach->cmd_buf = bu;
-}
-}
-}
-return (r);
+		if (r > 0)
+		{
+			if ((*bu)[r - 1] == '\n')
+			{
+				(*bu)[r - 1] = '\0'; /* remove trailing newline */
+				r--;
+			}
+			tach->linecount_flag = 1;
+			remove_comments(*bu);
+			build_history_list(tach, *bu, tach->histcount++);
+			/* if (_strchr(*bu, ';')) is this a command chain? */
+			{
+				*leno = r;
+				tach->cmd_buf = bu;
+			}
+		}
+	}
+	return (r);
 }
 
 /**
@@ -50,105 +52,106 @@ return (r);
 */
 ssize_t get_input(info_t *tach)
 {
-static char *buf; 
-static size_t i, j, len;
-ssize_t r = 0;
-char **buf_p = &(tach->arg), *p;
+	static char *buf; /* the ';' command chain buffer */
+	static size_t i, j, len;
+	ssize_t r = 0;
+	char **buf_p = &(tach->arg), *p;
 
-_putchar(BUF_FLUSH);
-r = input_buf(tach, &buf, &len);
-if (r == -1) 
-return (-1);
-if (len)
-j = i; 
-p = buf + i;
-check_chain(tach, buf, &j, i, len);
-while (j < len)
-{
-if (is_chain(tach, buf, &j))
-break;
-j++;
-}
+	_putchar(BUF_FLUSH);
+	r = input_buf(tach, &buf, &len);
+	if (r == -1) /* EOF */
+		return (-1);
+	if (len)	/* we have commands left in the chain buffer */
+	{
+		j = i; /* init new iterator to current buf position */
+		p = buf + i; /* get pointer for return */
 
-i = j + 1; 
-if (i >= len) 
-{
-i = len = 0; 
-tach->cmd_buf_type = CMD_NORM;
-}
+		check_chain(tach, buf, &j, i, len);
+		while (j < len) /* iterate to semicolon or end */
+		{
+			if (is_chain(tach, buf, &j))
+				break;
+			j++;
+		}
 
-*buf_p = p; 
-return (_strlen(p)); 
-}
+		i = j + 1; /* increment past nulled ';'' */
+		if (i >= len) /* reached end of buffer? */
+		{
+			i = len = 0; /* reset position and length */
+			tach->cmd_buf_type = CMD_NORM;
+		}
 
-*buf_p = buf; 
-return (r); 
-}
+		*buf_p = p; /* pass back pointer to current command position */
+		return (_strlen(p)); /* return length of current command */
+	}
 
-/**
-* read_buf - reads a buffer
-* @tach: param
-* @bu: buffer
-* @size: size deyal
-*
-* Return: r
-*/
-ssize_t read_buf(info_t *tach, char *bu, size_t *size)
-{
-ssize_t r = 0;
-
-if (*size)
-return (0);
-r = read(tach->readfd, bu, READ_BUF_SIZE);
-if (r >= 0)
-*size = r;
-return (r);
+	*buf_p = buf; /* else not a chain, pass back buffer from _getline() */
+	return (r); /* return length of buffer from _getline() */
 }
 
 /**
 * _getline - gets the next line of input from STDIN
 * @tach: param
 * @pitro: addressL
-* @leno: size 
+* @leno: size
+* Return: r
+*/
+ssize_t read_buf(info_t *tach, char *bu, size_t *size)
+{
+	ssize_t r = 0;
+
+	if (*size)
+		return (0);
+	r = read(tach->readfd, bu, READ_BUF_SIZE);
+	if (r >= 0)
+		*size = r;
+	return (r);
+}
+
+/**
+* _getline - gets the next line of input from STDIN
+* @tach: param
+* @pitro: addressL
+* @leno: size
 * Return: s
 */
 int _getline(info_t *tach, char **pitro, size_t *leno)
 {
-static char buf[READ_BUF_SIZE];
-static size_t i, len;
-size_t k;
-ssize_t r = 0, s = 0;
-char *p = NULL, *new_p = NULL, *c;
+	static char buf[READ_BUF_SIZE];
+	static size_t i, len;
+	size_t k;
+	ssize_t r = 0, s = 0;
+	char *p = NULL, *new_p = NULL, *c;
 
-p = *pitro;
-if (p && leno)
-s = *leno;
-if (i == len)
-i = len = 0;
+	p = *pitro;
+	if (p && leno)
+		s = *leno;
+	if (i == len)
+		i = len = 0;
 
-r = read_buf(tach, buf, &len);
-if (r == -1 || (r == 0 && len == 0))
-return (-1);
+	r = read_buf(tach, buf, &len);
+	if (r == -1 || (r == 0 && len == 0))
+		return (-1);
 
-c = _strchr(buf + i, '\n');
-k = c ? 1 + (unsigned int)(c - buf) : len;
-new_p = _realloc(p, s, s ? s + k : k + 1);
-if (!new_p) /* MALLOC FAILURE! */
-return (p ? free(p), -1 : -1);
+	c = _strchr(buf + i, '\n');
+	k = c ? 1 + (unsigned int)(c - buf) : len;
+	new_p = _realloc(p, s, s ? s + k : k + 1);
+	if (!new_p) /* MALLOC FAILURE! */
+		return (p ? free(p), -1 : -1);
 
-if (s)
-_strncat(new_p, buf + i, k - i);
-else
-_strncpy(new_p, buf + i, k - i + 1);
+	if (s)
+		_strncat(new_p, buf + i, k - i);
+	else
+		_strncpy(new_p, buf + i, k - i + 1);
 
-s += k - i;
-i = k;
-p = new_p;
+	s += k - i;
+	i = k;
+	p = new_p;
 
-if (leno)
-*leno = s;
-*pitro = p;
-return (s);
+	if (leno)
+		*leno = s;
+	*pitro = p;
+	return (s);
 }
 
 /**
@@ -159,7 +162,7 @@ return (s);
 */
 void sigintHandler(__attribute__((unused))int s_num)
 {
-_puts("\n");
-_puts("$ ");
-_putchar(BUF_FLUSH);
+	_puts("\n");
+	_puts("$ ");
+	_putchar(BUF_FLUSH);
 }
