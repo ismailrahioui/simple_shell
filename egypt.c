e2acec673928739a961d2dfe4f8543efe845f85e
fadymaged129 <fadymaged129@gmail.com>
2023-07-28 07:29:14 -0700

    solve some errors

--- egypt.c
+++ egypt.c
@@ -21,13 +21,10 @@ list_t *add_node(list_t **addpoint, const char *stfield, int raqm)
 	new_head -> raqm = raqm;
 	if (stfield)
 	{
-<<<<<<< HEAD
 		new_head->stfield = _strdup(stfield);
 		if (!new_head->stfield)
-=======
-		new_head -> stfield = _strdup(str);
-		if (!new_head->str)
->>>>>>> 7d51b57437715a47cfbc909494d35f3dc78c399f
+			new_head -> stfield = _strdup(stfield);
+		if (!new_head->stfield)
 		{
 			free(new_head);
 			return (NULL);
